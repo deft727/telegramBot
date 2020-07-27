3adba7058181bec927d2018fd1c344cf81824a02
deft727 <deft727@gmail.com>
2020-07-27 12:43:15 +0300

    tbot from parfumelover

---
+++ bot.py
@@ -0,0 +1,70 @@
+import config
+import logging
+import asyncio
+from datetime import datetime
+from aiogram import Bot,Dispatcher,executor,types
+from sqliter import SQLiter
+from pars import StopGame
+logging.basicConfig(level=logging.INFO)
+
+bot=Bot(token=config.API_TOKEN)
+dp=Dispatcher(bot)
+
+db = SQLiter('db.db')
+sg=StopGame('lastkey.txt')
+
+@dp.message_handler(commands=['subscribe'])
+async def subscribe(message: types.Message):
+	if(not db.subscriber_exists(message.from_user.id)):
+		db.add_subscriber(message.from_user.id)
+	else:
+		db.update_subscription(message.from_user.id, True)
+	
+	await message.answer("Вы успешно подписались на Новинки!")
+
+@dp.message_handler(commands=['unsubscribe'])
+async def unsubscribe(message: types.Message):
+	if(not db.subscriber_exists(message.from_user.id)):
+		db.add_subscriber(message.from_user.id, False)
+		await message.answer("Вы и так не подписаны.")
+	else:
+		db.update_subscription(message.from_user.id, False)
+
+		await message.answer("Вы успешно отписаны от рассылки.")
+
+async def scheduled(wait_for):
+
+	while True:
+
+		await asyncio.sleep(wait_for)
+
+		# проверяем наличие новых игр
+		new_games = sg.new_games()
+
+		if(new_games):
+			# если игры есть, переворачиваем список и итерируем
+			# new_games.reverse()
+			for ng in new_games:
+				nfo = sg.game_info(ng)
+
+				# получаем список подписчиков бота
+				subscriptions = db.get_subscriptions()
+
+				# отправляем всем новость
+				for s in subscriptions:
+					with open(sg.download_image(nfo['image']), 'rb') as photo:
+						await bot.send_photo(
+							s[1],
+							photo,
+							caption = nfo['title'] + "\n"  + nfo['excerpt'] + "\n" + nfo['price'] + "\n\n" + nfo['link'],
+							disable_notification = True
+						)
+
+				
+				# обновляем ключ
+				sg.update_lastkey(nfo['id'])
+
+# запускаем лонг поллинг
+if __name__ == '__main__':
+	dp.loop.create_task(scheduled(10)) # пока что оставим 10 секунд (в качестве теста)
+	executor.start_polling(dp, skip_updates=True)
---
+++ pars.py
@@ -0,0 +1,95 @@
+import re
+import os.path
+import requests
+from bs4 import BeautifulSoup as BS
+from urllib.parse import urlparse
+
+class StopGame:
+	host = 'https://parfumelover.herokuapp.com'
+	url = 'https://parfumelover.herokuapp.com'
+	lastkey = ""
+	lastkey_file = ""
+
+	def __init__(self, lastkey_file):
+		self.lastkey_file = lastkey_file
+
+		if(os.path.exists(lastkey_file)):
+			self.lastkey = open(lastkey_file, 'r').read()
+		else:
+			f = open(lastkey_file, 'w')
+			self.lastkey = self.get_lastkey()
+			f.write(self.lastkey)
+			f.close()
+
+	def new_games(self):
+		r = requests.get(self.url)
+		html = BS(r.content, 'html.parser')
+
+		new = []
+		items = html.select('.info-wrap > .fix-height >  a')
+		for i in items:
+			key = int((i['href'].replace('/show?id=','')))
+			if(int(self.lastkey) < int(key)):
+				new.append(i['href'])
+
+		return new
+
+	def game_info(self, uri):
+		link = self.host + uri
+		while True:
+			try:
+				r = requests.get(link)
+				break
+			except:
+				r = requests.get(link)
+		html = BS(r.content, 'html.parser')
+
+		# poster =html.select('.img-wrap > .img-fluid')[1]['src']
+		
+		poster=html.select('.img-fluid')[0]['src']
+		
+		# 'https://aromateque.com.ua/media/catalog/product/cache/1/small_image/174x222/9df78eab33525d08d6e5fb8d27136e95/e/a/eau_sauvage_eau_de_toilette-50_2.jpg'
+		# html.select('.img-fluid')[0]['src']
+		info = {
+			"id": uri.replace('/show?id=','').strip(),
+			"title": html.select('.p-4')[0].text.split('\n')[1],
+			"link": link,
+			"image": poster,
+			# 'https://parfumelover.herokuapp.com/',+poster,
+			"excerpt":html.select('.p-4')[0].text.split('\n')[2],
+			"price":html.select('.display-4')[0].text
+		};
+
+		return info
+
+	def download_image(self, url):
+		r = requests.get(url, allow_redirects=True)
+
+		a = urlparse(url)
+		filename = os.path.basename(a.path)
+		open(filename, 'wb').write(r.content)
+
+		return filename
+
+
+	def get_lastkey(self):
+		r = requests.get(self.url)
+		html = BS(r.content, 'html.parser')
+
+		items = html.select('.info-wrap > .fix-height >  a')
+		return items[0]['href'].replace('/show?id=','')
+
+	def parse_href(self, href):
+		result = href['href'].replace('/show?id=','')
+		return result
+
+	def update_lastkey(self, new_key):
+		self.lastkey = new_key
+
+		with open(self.lastkey_file, "r+") as f:
+			data = f.read()
+			f.seek(0)
+			f.write(str(new_key))
+			f.truncate()
+
+		return new_key
